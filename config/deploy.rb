912a5c757757d06c5386f772f0d64f60f356fe1b
Adam Hooper <adam@adamhooper.com>
2008-05-19 17:52:10 -0400

    Set up database.yml

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,3 +1,5 @@
+require 'erb'
+
 set :application, "ngodb"
 set :repository,  "git://github.com/adamh/ngodb.git"
 set :deploy_to, "/home/adam/rails/#{application}"
@@ -22,5 +24,42 @@ namespace :deploy do
   desc "Restarts the server"
   task :restart, :roles => :app do
     run %(touch #{release_path}/tmp/restart.txt)
+  end
+end
+
+before "deploy:setup", :db
+after "deploy:update_code", "db:symlink" 
+
+# Copied from http://www.shanesbrain.net/2007/5/30/managing-database-yml-with-capistrano-2-0
+namespace :db do
+  desc "Create database yaml in shared path" 
+  task :default do
+    db_config = ERB.new <<-EOF
+base: &base
+  adapter: mysql
+  socket: /tmp/mysql.sock
+  username: #{user}
+  password: #{password}
+
+development:
+  database: #{application}_dev
+  <<: *base
+
+test:
+  database: #{application}_test
+  <<: *base
+
+production:
+  database: #{application}_prod
+  <<: *base
+EOF
+
+    run "mkdir -p #{shared_path}/config" 
+    put db_config.result, "#{shared_path}/config/database.yml" 
+  end
+
+  desc "Make symlink for database yaml" 
+  task :symlink do
+    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml" 
   end
 end
