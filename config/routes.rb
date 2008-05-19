3dcce9caa60eeba9d88493c9e00d3db41552d623
Adam Hooper <adam@adamhooper.com>
2008-05-19 13:48:37 -0400

    Use path_prefix for admin pages, too

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,16 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :csos, :path_prefix => ':locale'
   map.resources :sessions
 
-  map.namespace :admin do |admin|
-    admin.resources :annual_budgets
-    admin.resources :funding_sources
-    admin.resources :membership_types
-    admin.resources :operational_areas
-    admin.resources :registration_acts
-    admin.resources :sector_focuses
-    admin.resources :target_groups
+  map.with_options :path_prefix => ':locale/admin' do |m|
+    m.namespace :admin do |admin|
+      admin.resources :annual_budgets
+      admin.resources :funding_sources
+      admin.resources :membership_types
+      admin.resources :operational_areas
+      admin.resources :registration_acts
+      admin.resources :sector_focuses
+      admin.resources :target_groups
+    end
   end
 
   map.root :controller => 'csos', :locale => 'en'
